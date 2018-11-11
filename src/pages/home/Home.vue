b9acc6950e5bed215248eedd792289f3b1abcfdd
苏伟 <1751086138@qq.com>
2018-11-12 03:11:18 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,11 +1,15 @@
 <template>
     <div>
-        helloworld
+        <home-header></home-header>
     </div>
 </template>
 <script>
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 <style>
